9800aab888aa972516f9adfb851c820ce819878f
vineetjnv2780-star <vineetjnv2780@gmail.com>
2025-12-29 02:09:55 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // Added base for GitHub Pages support
+      base: '/Torch/', 
       server: {
         port: 3000,
         host: '0.0.0.0',
